a5ba0dd826246d6fb314c190b5bc755195c98a6b
Mrinalsharma1 <checkkro1@gmail.com>
2022-12-03 11:21:04 +0530

    today we learn about comments, seperater, etc

---
+++ day2.py
@@ -0,0 +1,8 @@
+print("--------day2---------")
+print("hey this is \ndya2") #/n to write text into new line
+"""print("gdjsfsdghsdhsh")""" #this is also a way to comment code
+'''hey this is also a type of comment'''
+print('hey dude, he is a "good" boy')# this is a way to print "" in output
+print("cho ab \"laut\" chale")# this is also a way to print " " in output
+print("ye v\' dekho") #print single cote
+print("hey",6,7,sep="~",end=" ") #to seperate text with some symbol 
