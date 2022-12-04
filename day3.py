b0f6fb8881484b2a39202ab7351b133044ed3d68
Mrinalsharma1 <checkkro1@gmail.com>
2022-12-04 22:34:14 +0530

    today we learn about data types and list,tupple and dictonary etc

---
+++ day3.py
@@ -0,0 +1,26 @@
+print("--------day3---------")
+a=1 #it store the value as intiger
+print(type(a))#with type we can check the data type
+b=complex(8,3)#with complex we can set complex number
+print(type(b))
+c=1.1 #if we store decimal value it print type float
+print(type(c))
+d="mrinal" #the data type will be string
+print(type(d))
+e=None
+print(type(e)) #it will be print Nonetype 
+
+#list is mutable
+list1=[1,2,["sagar","mohan"],1.1,-6,[2,-8]] #we can define list inside list
+print(list1)
+
+#tupple is unmutable
+tupple1=("hey","this",("key",7),9,-8,True) # we can store diffirent data in tupple
+print(tupple1)
+
+#dictonary store value in the form of key:value
+disc={"name":"raju",
+      "class":"mca",
+      "roll":4,
+      "present":True} # we can store diffirent data in the form of key and value
+print(disc)
