6e1ee479686e128231012a49e74eab6542653920
Mrinalsharma1 <checkkro1@gmail.com>
2022-12-07 17:23:07 +0530

    today we learn about how to take user input

---
+++ day6.py
@@ -0,0 +1,7 @@
+print("-----------Day6----------")
+x=input("First Number : ")#input only takes string value if we take x=10
+y=input("second number : ")# y=33
+print(x+y)#output woude be 1033
+
+print(int(x)+int(y))# after typecasting the result will be 32
+print(float(x)+int(y)) # now the output will be 32.0
