665b35fb5c82c74964c84930cc57937c7f3ed942
Mrinalsharma1 <checkkro1@gmail.com>
2022-12-14 19:25:16 +0530

    today we learn about for loop and few more concepts like range etc

---
+++ day13.py
@@ -0,0 +1,38 @@
+print("-----------Day13----------")
+# a="mrinal"
+# for i in a:
+#     print(i,end=", ") #end is used to seperate the value at the end
+
+# for i in a:
+#     if i=="a":
+#         print(i,"is avalible")
+#     print(i)
+
+# e=input("enter element : ")
+# s=input("enter char which you want to search : ")
+# count=0
+# t=0
+# for i in e:
+#     t+=1
+#     if i==s:
+#         print(i,"found at ",t)
+#         count+=1
+# print(count)
+
+# list1=["raju","mukesh","mona","sona","priya"]
+# n=input("enter searching name : ")
+# for i in list1:
+#     if i==n:
+#         print(i,"Found ")
+#     for j in i:
+#         print(j)
+        
+        
+# for i in range(5):#it start from 0 index
+#     print(i+1)
+
+# for i in range(0,5): #both snipt have same work
+#     print(i)
+    
+for i in range(0,6,3): #its means 0=start, 6=end, 3=steps so in third steps it's reach 6 so it wont print
+    print(i)
