a14b7e2a91206344908672aa2f5f665a74fc5aff
Mrinalsharma1 <checkkro1@gmail.com>
2022-12-15 19:15:44 +0530

    today we learn about while loop in python

---
+++ day14.py
@@ -0,0 +1,17 @@
+print("-----------Day14----------")
+#while loop
+# n=int(input("Enter number : "))
+# while (n<=10):
+#     print("value is ",n)
+#     n+=1
+    
+n=5
+while(n<10):
+    if(n>5 and n<10):
+        print("range b/w 5 to 10")
+    elif(n<5):
+        print("n below 5")
+    else:
+        print("max num")
+    n+=1
+print("done")
