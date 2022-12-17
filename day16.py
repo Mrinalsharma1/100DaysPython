7174cd712ad31966663bb585a1aad62e89448e59
Mrinalsharma1 <checkkro1@gmail.com>
2022-12-18 00:30:43 +0530

    learn about function in python

---
+++ day16.py
@@ -0,0 +1,18 @@
+print("-----------Day16----------")
+# def calculateGame(a,b):
+#     mean=(a*b)/(a+b)
+#     print(mean)
+    
+# a=9
+# b=8
+# def demo(a,b):
+#     pass #it means i will define this function later
+# if(a>b):
+#     print("a is greater")
+# else:
+#     print("b is greater")
+# calculateGame(a,b)
+
+def cal(a,b):
+    return a+b
+print(cal(3,4))
