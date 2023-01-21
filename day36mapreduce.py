cb642c61d61c523bec3971db8f7905fa2ad58ebf
Mrinalsharma1 <checkkro1@gmail.com>
2023-01-21 11:49:40 +0530

    today we learn about lamda function and is and == diffirent

--- day36mapreduce.py
+++ day36mapreduce.py
@@ -1,12 +1,13 @@
 cube=lambda a:(a*a*a)
 l=[1,1,2,3,4,5]
+print(l)
 newl=[]
 # for item in l:
 #     newl.append(cube(item))
 # print(newl) #without map we can cube the list item
 
 # newl=list(map(cube,l)) #with map we are doing same job
-print(list(map(cube,l))) #we can write in one line also
+# print(list(map(cube,l))) #we can write in one line also
 
 #filter
 def filter_fun(a): #we can write filter with this way
@@ -16,4 +17,17 @@ ne=list(filter(f,l)) #function inside function pasing
 # print(ne)
 
 #we can do same filter in one line also
-print(list(filter(lambda a:a>2,l)))
+# print(list(filter(lambda a:a>2,l)))
+
+# reducer function in python
+from functools import reduce
+
+def sum(x,y): #one way to add number
+    return (x+y)
+
+a=lambda x,y:(x+y) #with lamda 
+# sum=reduce(a,l)
+
+sum=reduce((lambda x,y:(x+y)),l) #recucer and lamda in single line
+
+print(reduce((lambda x,y:(x+y)),l))
---
+++ day37is.py
@@ -0,0 +1,11 @@
+# diffirent between == and is
+a=[12,23,33]
+b=[12,23,33]
+
+print(a==b) #it compare value and return true
+print(a is b) #it's compare the memory location of both list bcus list is mutable so it return false
+
+a=3
+b=3
+print(a==b)
+print(a is b)
