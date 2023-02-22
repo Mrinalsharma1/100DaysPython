bfc48e812f2d8b4d2cadfc7444c0d210f90f4f48
Mrinalsharma1 <checkkro1@gmail.com>
2023-02-22 17:45:09 +0530

    warsu operator in python

---
+++ day53warlusopt.py
@@ -0,0 +1,13 @@
+# it is a normal or tradictional way
+foods=list()
+while True:
+    x=input("Enter What Food you Like? : ")
+    if x=='quit':
+        print(foods)
+        break
+    foods.append(x)
+    
+#using warsu operator
+foods1=list()
+while(name:= input("Enter What Food you Like? : ")) !="quit": 
+    foods1.append(name)
