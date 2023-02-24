76e5df3887002b2a062345d9fb1bc45919b050e0
Mrinalsharma1 <checkkro1@gmail.com>
2023-02-24 20:48:55 +0530

    web scraping | printing data of any website | request & BeautifulSoup packages

---
+++ day55requestmodel.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+# res=requests.get("https://www.codewithharry.com") #get data from this url
+# print(res.text) #print the code of this url
+
+url1="https://alphaworldtech.com"
+r=requests.get(url1)
+
+soup=BeautifulSoup(r.text,'html.parser') #it is used the scrape the data from web
+# print(soup.prettify())
+for heading in soup.find_all('p'): #here we are finding the data from p tag
+    print(heading.text) #printing the data from p tag
+
+# url="https://jsonplaceholder.typicode.com/posts"
+# data={
+#     "title":'foo',
+#     "body":'bar',
+#     'userId':1,
+# }
+# headers={
+#     'Content-type':'application/json; charset=UTF-8'
+# }
+# resp=requests.post(url,headers=headers,json=data) #get request
--- ex8textspeech.py
+++ ex8textspeech.py
@@ -1,14 +1,21 @@
-from gtts import gTTS
-from threading import Timer 
-import os
-data=["raman","raju","sihla","monika","sanaya"]
+# from gtts import gTTS
+# from threading import Timer 
+# from os import system
+import win32com.client
+data=["ramu","niss","nishi","monika","sanaya"]
 
-def exec_func(i):
-    mytext = f"Hi {i} , this is an example of converting text to audio. This is a bot speaking here, not a real human!"
-    audio = gTTS(text=mytext, lang="en", slow=False)
-    audio.save("example.mp3")
-    # playsound("example.mp3")
-    os.system("start example.mp3")
+speaker=win32com.client.Dispatch("SAPI.SpVoice") #speak names
+
+for name in data:
+    speaker.Speak(f'Welcome ,{name}')
+
+# def exec_func(i):
+#     mytext = f"Hi {i} , this is an example of converting text to audio. This is a bot speaking here, not a real human!"
+#     audio = gTTS(text=mytext, lang="en", slow=False)
+#     audio.save("example.mp3")
+#     # playsound("example.mp3")
+#     os.system("start example.mp3")
  
-for i in data:   
-    Timer(100, exec_func(i)).start()
+# for i in data:   
+#     Timer(100, exec_func(i)).start()
+
