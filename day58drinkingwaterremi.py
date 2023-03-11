5a58e2df3d4551c61bbfe32bc2330a166fdc5269
Mrinalsharma1 <checkkro1@gmail.com>
2023-03-11 10:25:02 +0530

    add notification features on reminder project

--- day58drinkingwaterremi.py
+++ day58drinkingwaterremi.py
@@ -1,13 +1,18 @@
 import win32com.client
+from plyer import notification
 import time
 
 def reminder():
     speaker=win32com.client.Dispatch("SAPI.SpVoice") #speak names
+    notification.notify(
+        title = "General Reminder",
+        message = "stop doing work and drink a ship of water",
+        timeout = 20
+    )
     speaker.Speak('stop doing work and drink a ship of water ')
 
 starttime = time.time()
 while True:
-    print("tick")
     reminder()
     time.sleep(60)
     # time.sleep(60.0 - ((time.time() - starttime) % 60.0))
